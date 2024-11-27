87bba4a2e32c38a12339ce2ca444a8eb22ea2fe8
Kavya <159226897+jyothinavya23@users.noreply.github.com>
2024-11-27 11:25:18 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -2,6 +2,6 @@ public class simple
 {
 	public static void main(String[]args)
 	{
-		System.out.println(welcome to tumkuru,mysore,benglore");
+		System.out.println("welcome to tumkuru cold season");
 	}
 }
